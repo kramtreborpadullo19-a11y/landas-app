c7640af6e65123267452a5b9edeec3f2ff47181f
kramtreborpadullo19-a11y <kramtreborpadullo19@gmail.com>
2026-08-16 09:07:11 +0800

    move www intop backend for Railway deploy

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '..', 'www')));
+app.use(express.static(path.join(__dirname, 'www')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'www', 'index.html'));
+  res.sendFile(path.join(__dirname, 'www', 'index.html'));
 });
 
 app.get('/api/health', (req, res) => res.json({ ok: true }));
